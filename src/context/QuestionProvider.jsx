3522c8877339cfb2efaa9d986426868560d578ba
Mehrdad <mehrdadkf9@gmail.com>
2022-04-17 22:25:32 +0430

    Add question provider to project

---
+++ src/context/QuestionProvider.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+import { postQuestion } from "../api/api";
+import { getQuestions } from "../api/api";
+
+const QuestionsContext = createContext();
+const QuestionsContextDispatcher = createContext();
+
+const QuestionsProvider = ({ children }) => {
+  const [questions, setQuestions] = useState([]);
+
+  return (
+    <QuestionsContext.Provider value={questions}>
+      <QuestionsContextDispatcher.Provider value={setQuestions}>
+        {children}
+      </QuestionsContextDispatcher.Provider>
+    </QuestionsContext.Provider>
+  );
+};
+
+export default QuestionsProvider;
+
+export const useQuestions = () => useContext(QuestionsContext);
+
+export const useQuestionsActions = () => {
+  const setQuestions = useContext(QuestionsContextDispatcher);
+
+  const addQuestion = (comment) => {
+    postQuestion(comment)
+      .then(() => getQuestions())
+      .then((res) => setQuestions(res.data));
+  };
+
+  const updateQuestions = (data) => {
+    setQuestions(data);
+  };
+
+  return { addQuestion, updateQuestions };
+};
